c732e1015918e3360ae5da63cb2bb729be087109
alschofield93 <alschofield93@gmail.com>
2021-03-04 21:50:53 -0500

    mongo connection and cors

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express')
-const helmet = require("helmet")
+const helmet = require('helmet')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-var url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y11xi.mongodb.net/${process.env.MONGO_NAME}?retryWrites=true&w=majority`;
+var url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y11xi.mongodb.net?retryWrites=true&w=majority`;
 
 const client = new MongoClient(url);
 
 const app = express()
 
 app.use(helmet())
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -40,13 +45,11 @@ app.post('/comments', (req, res) => {
     //    - type
     //    - sub comments
 
-    collection.updateOne({ _id : req.body._id }, { $set: { ...req.body } }, { upsert: true }, (_e) => {
+    collection.updateOne({ _id : req.body._id }, { $set: { ...req.body } }, { upsert: true }, (_e, resp) => {
       if (_e) {
         res.json(_e);
       } else {
-        res.json({
-          status: 'created'
-        });
+        res.json(resp);
       }
     })
   })
