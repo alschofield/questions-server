e2536ebc38c69790f029dace1a0daf6649cf28a7
alschofield93 <alschofield93@gmail.com>
2021-03-06 22:11:00 -0500

    hosting changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID;
 var url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y11xi.mongodb.net?retryWrites=true&w=majority`;
 
 const client = new MongoClient(url);
@@ -12,11 +12,15 @@ const app = express()
 app.use(helmet())
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'https://questions.alschofield.vercel.app/'
 }))
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.json("https://www.youtube.com/watch?v=OnG7oL9Gg4o")
+})
 
 app.get('/comments', (req, res) => {
   client.connect((e) => {
@@ -33,19 +37,14 @@ app.get('/comments', (req, res) => {
   })
 })
 
-app.post('/comments', (req, res) => {
+app.post('/comments/:id', (req, res) => {
   client.connect((e) => {
     const db = client.db('comments')
     const collection = db.collection('comments')
 
-    // field validation
-    //  - only allow
-    //    - text/value
-    //    - date
-    //    - type
-    //    - sub comments
+    const _id = (req.params.id === 'new') ? new ObjectID() : req.params.id
 
-    collection.updateOne({ _id : req.body._id }, { $set: { ...req.body } }, { upsert: true }, (_e, resp) => {
+    collection.updateOne({ _id : _id }, { $set: { ...req.body } }, { upsert: true }, (_e, resp) => {
       if (_e) {
         res.json(_e);
       } else {
@@ -55,6 +54,6 @@ app.post('/comments', (req, res) => {
   })
 })
 
-app.listen(8000, () => {
-  console.log('comments service listening on 8000')
+app.listen(8080, () => {
+  console.log('comments service listening on 8080')
 })
